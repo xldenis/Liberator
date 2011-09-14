15d8e643e55a0afd7f5eca1afa839f68929d87d0
Xavier Denis <xldenis@gmail.com>
2011-09-14 11:03:05 -0500

    Finish seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@ cat = Category.create(:name =>  "News", :front_page_articles => nil)
 15.times do |index|
   cat.articles.create(:title => "Article #{index}",:slug => "Slug #{index}",:content => "test")
 end
+User.create(:email => "reporter@liberator.com", :encrypted_password=> "$2a$10$oUDLvCUVDVS2EuJFem//yOF.RJxnRgE4uFaVJvj35MD3hvIBV7RUK", :name => "Reporter User", :role => "reporter")
+User.create(:email => "admin@liberator.com", :encrypted_password=> "$2a$10$njr3d3a.NxH8YiE0HJRbG.lDNJ7lvkYS3X2ptS.6y19gDbaff14aK",:name => "Admin User", :role => "admin")
