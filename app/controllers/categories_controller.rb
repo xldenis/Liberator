d850b14735b36640c9ab1f8ab1ae64ac639e39d7
Xavier Denis <xldenis@gmail.com>
2011-09-19 16:01:53 -0500

    Fix Category#show, finally. Create Category.fron_to_articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,14 @@
 class CategoriesController < ApplicationController
   def home
   @category = Category.first
-  @articles=[]
-  if @category.front_page_articles
-  @category.front_page_articles.each do |article|
-    if !(article=="nil") then @articles<<Article.find(article) else @articles<<nil end
-  end
-  else
-  @articles = nil
-  end
+  @articles = @category.front_to_articles
   end
   def index
   @categories = Category.all
   end
   def show
   @category = Category.find(params[:id])
-   if @category.front_page_articles
-
-     @front_page_articles = []
-       @category.front_page_articles.each do |article|
-         if !(article=="nil")&&article then @front_page_articles<<Article.find(article) else @front_page_articles<<nil end
-           end
-             else
-
-               @front_page_articles = nil
-                 end
-
+  @articles = @category.front_to_articles
   end
   def edit 
   @category = Category.find(params[:id])
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,18 @@ class Category
   has_many :articles
   field :name
   field :front_page_articles, :type=> Array
+def front_to_articles
+  articles=[]
+    if self.front_page_articles
+      self.front_page_articles.each do |article|
+    if !(article=="nil") then articles<<Article.find(article) else articles<<nil end
+  end
+  else
+    articles = nil
+  end
+  articles
+end
+def stuff
+articles = Article.first
+end
 end
