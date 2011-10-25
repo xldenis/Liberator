ded14a249129f6b75a2c5b21b5f0f5d7fbf77775
Xavier Denis <xldenis@gmail.com>
2011-10-25 13:08:37 -0500

    stashing

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,4 @@
 class Picture
   include Mongoid::Document
-  include Mongoid::Paperclip
-embedded_in :user
 
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,4 @@
 CarrierWave.configure do |config|
 config.cache_dir = "uploads"
+require 'carrierwave/processing/mini_magick'
 end
